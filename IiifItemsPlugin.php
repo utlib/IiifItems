525b9ea913f01a66f4fc56700b926fc5e2383c98
Dickson Law <dickson.law@utoronto.ca>
2016-11-23 11:18:24 -0500

    Fixed broken upward migration in 0.0.1.4

--- IiifItemsPlugin.php
+++ IiifItemsPlugin.php
@@ -117,6 +117,14 @@ class IiifItemsPlugin extends Omeka_Plugin_AbstractPlugin
                 `added` timestamp DEFAULT '2016-11-01 00:00:00',
                 `modified` timestamp DEFAULT NOW() ON UPDATE NOW()
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+            $db->query("CREATE TABLE IF NOT EXISTS `{$db->prefix}iiif_items_cached_json_data` (
+                `id` int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                `record_id` int(11) NOT NULL,
+                `record_type` varchar(50) NOT NULL,
+                `url` varchar(255) NOT NULL,
+                `data` text NOT NULL,
+                `generated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
         }
         
         public function hookUninstall() {
@@ -155,6 +163,7 @@ class IiifItemsPlugin extends Omeka_Plugin_AbstractPlugin
             // Drop tables
             $db = $this->_db;
             $db->query("DROP TABLE IF EXISTS `{$db->prefix}iiif_items_job_statuses`;");
+            $db->query("DROP TABLE IF EXISTS `{$db->prefix}iiif_items_cached_json_data`;");
         }
         
         public function hookUpgrade($args) {
