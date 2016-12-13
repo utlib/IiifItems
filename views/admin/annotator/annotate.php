0246fde100db61008e235e4f1c5cff0c2f07e21f
Dickson Law <dickson.law@utoronto.ca>
2016-12-13 14:26:41 -0500

    Added back button on annotator screen

--- views/admin/annotator/annotate.php
+++ views/admin/annotator/annotate.php
@@ -1,12 +1,13 @@
 <?php
     $title = metadata($thing, array('Dublin Core', 'Title'));
     echo head(array(
-        'title' => $title,
+        'title' => "Annotating: \"{$title}\"",
     ));
 ?>
 <?php
 	$urlJs = src('mirador', 'js/mirador', 'js');
 	$urlCss = src('mirador-combined', 'js/mirador/css', 'css');
 ?>
+<a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" class="green button">Return to Previous Page</a>
 <iframe src="<?php echo absolute_url(array('things' => $type, 'id' => $thing->id), 'iiifitems_annotator'); ?>" allowfullscreen="allowfullscreen" style="width:100%; height:600px;"></iframe>
 <?php echo foot(); ?>
