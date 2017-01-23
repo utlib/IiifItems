89445d3c4f316196df42109852b6f779caf3fe8a
Dickson Law <dickson.law@utoronto.ca>
2017-01-23 14:01:34 -0500

    Added ability to fill in templates for the IIIF prefix, needed for integration with our Loris server

--- forms/Config.php
+++ forms/Config.php
@@ -9,13 +9,13 @@ class IiifItems_Form_Config extends Omeka_Form {
         // Enable automated HTTP-IIIF Bridge
         $this->addElement('text', 'iiifitems_bridge_prefix', array(
             'label' => __('IIIF Prefix'),
-            'description' => __('The URL root of the HTTP-resolved IIIF server referencing this Omeka installation'),
+            'description' => __('The URL root of the HTTP-resolved IIIF server referencing this Omeka installation. Use {FILENAME} for a file name without the extension, {EXTENSION} for the file extension alone and {FULLNAME} for a file name with the extension.'),
             'value' => get_option('iiifitems_bridge_prefix'),
         ));
         // Mirador
         $this->addElement('text', 'iiifitems_mirador_path', array(
             'label' => __('Mirador Path'),
-            'description' => __('URL to the directory holding the main mirador.js and supporting files'),
+            'description' => __('URL to the directory holding the main mirador.js and supporting files.'),
             'value' => get_option('iiifitems_mirador_path'),
         ));
     }
