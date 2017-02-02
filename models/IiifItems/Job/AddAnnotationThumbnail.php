6ab22d1e631d88e52eb640e8a6eb988683f6539f
Dickson Law <dickson.law@utoronto.ca>
2017-02-02 16:33:12 -0500

    Fixed important bug that caused annotation thumbnails to fail

--- models/IiifItems/Job/AddAnnotationThumbnail.php
+++ models/IiifItems/Job/AddAnnotationThumbnail.php
@@ -26,11 +26,11 @@ class IiifItems_Job_AddAnnotationThumbnail extends Omeka_Job_AbstractJob {
                 $prefix = $fileJsonData['resource']['service']['@id'];
                 $suffix = $this->__getIiifImageSuffix($fileJsonData);
             } else {
-                $prefix = rtrim(get_option('iiifitems_bridge_prefix'), '/') . '/' . $originalFile->filename;
+                $prefix = rtrim(IiifItems_CanvasUtil::fileIiifPrefix($originalFile), '/');
                 $suffix = 'default.jpg';
             }
         } else {
-            $prefix = rtrim(get_option('iiifitems_bridge_prefix'), '/') . '/' . $originalFile->filename;
+            $prefix = rtrim(IiifItems_CanvasUtil::fileIiifPrefix($originalFile), '/');
             $suffix = 'default.jpg';
         }
         $this->__downloadIiifImageToItem($annotationItem, $prefix, $dims, $suffix);
@@ -61,17 +61,22 @@ class IiifItems_Job_AddAnnotationThumbnail extends Omeka_Job_AbstractJob {
             switch ($image['resource']['service']['@context']) {
                 case 'http://library.stanford.edu/iiif/image-api/1.1/conformance.html#level1':
                 case 'http://iiif.io/api/image/1/context.json':
+                case 'https://library.stanford.edu/iiif/image-api/1.1/conformance.html#level1':
+                case 'https://iiif.io/api/image/1/context.json':
                     return 'native.jpg';
                 case 'http://iiif.io/api/image/2/context.json':
+                case 'https://iiif.io/api/image/2/context.json':
                     return 'default.jpg';
             }
             switch ($image['resource']['service']['profile']) {
                 case 'http://library.stanford.edu/iiif/image-api/1.1/conformance.html#level1':
+                case 'https://library.stanford.edu/iiif/image-api/1.1/conformance.html#level1':
                     return 'native.jpg';
             }
         }
         catch (Exception $e) {
             return 'native.jpg';
         }
+        return 'native.jpg';
     }
 }
