616c5663a63531e2344b86d6228ce0b04b3984f9
Dickson Law <dickson.law@utoronto.ca>
2017-03-17 18:23:59 -0400

    Moved off Simple Pages integration, fixed bug with migrations

--- IiifItemsPlugin.php
+++ IiifItemsPlugin.php
@@ -56,6 +56,7 @@ class IiifItemsPlugin extends Omeka_Plugin_AbstractPlugin
         
         protected $_integrations = array(
             'ExhibitBuilder',
+            'SimplePages',
         );
         
         public function hookInstall() {
@@ -122,7 +123,7 @@ class IiifItemsPlugin extends Omeka_Plugin_AbstractPlugin
             }
             set_option('iiifitems_annotation_elements', json_encode($annotation_metadata_elements));
             // Add Annotation item type Text element
-            include_once(dirname(__FILE__) . '/migrations/0_0_1_5.php');
+            include_once(dirname(__FILE__) . '/libraries/IiifItems/Migration/0_0_1_5.php');
             $addTextElementMigration = new IiifItems_Migration_0_0_1_5();
             $addTextElementMigration->up();
             // Add IIIF server options
@@ -152,11 +153,11 @@ class IiifItemsPlugin extends Omeka_Plugin_AbstractPlugin
                 `generated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
             // Add media placeholder files
-            include_once(dirname(__FILE__) . '/migrations/0_0_1_7.php');
+            include_once(dirname(__FILE__) . '/libraries/IiifItems/Migration/0_0_1_7.php');
             $addMediaPlaceholdersMigration = new IiifItems_Migration_0_0_1_7();
             $addMediaPlaceholdersMigration->up();
             // Add UUIDs via a job
-            include_once(dirname(__FILE__) . '/migrations/0_0_1_6.php');
+            include_once(dirname(__FILE__) . '/libraries/IiifItems/Migration/0_0_1_6.php');
             $addUuidElementMigration = new IiifItems_Migration_0_0_1_6();
             $addUuidElementMigration->up();
         }
@@ -209,7 +210,7 @@ class IiifItemsPlugin extends Omeka_Plugin_AbstractPlugin
             $doMigrate = false;
             
             $versions = array();
-            foreach (glob(dirname(__FILE__) . '/migrations/*.php') as $migrationFile) {
+            foreach (glob(dirname(__FILE__) . '/libraries/IiifItems/Migration/*.php') as $migrationFile) {
                 $className = 'IiifItems_Migration_' . basename($migrationFile, '.php');
                 include $migrationFile;
                 $versions[$className::$version] = new $className();
@@ -271,11 +272,6 @@ class IiifItemsPlugin extends Omeka_Plugin_AbstractPlugin
                 $integration = new $integrationClass();
                 $integration->integrate();
             }
-            
-            add_shortcode('mirador_file', array($this, 'shortcodeMiradorFile'));
-            add_shortcode('mirador_items', array($this, 'shortcodeMiradorItems'));
-            add_shortcode('mirador_collections', array($this, 'shortcodeMiradorCollections'));
-            add_shortcode('mirador', array($this, 'shortcodeMirador'));
         }
         
         public function hookDefineRoutes($args) {
@@ -785,133 +781,6 @@ class IiifItemsPlugin extends Omeka_Plugin_AbstractPlugin
         public function inputForCollectionUuid($comps, $args) {
             $comps['input'] = $args['value'] ? $args['value'] : '&lt;TBD&gt;';
             return filter_minimal_input($comps, $args);
-        }
-        
-        /* Simple Pages shortcodes */
-        
-        public function shortcodeMiradorFile($args, $view) {
-            // Styles
-            $styles = isset($args['style']) ? $args['style'] : '';
-            // Single: View the file as-is
-            if (isset($args['id'])) {
-                $id = $args['id'];
-                $file = get_record_by_id('File', $id);
-                if ($file) {
-                    return '<iframe src="' . public_full_url(array('things' => 'files', 'id' => $id), 'iiifitems_mirador') . '" style="width:100%;height:400px;' . $styles . '"></iframe>';
-                }
-            }
-            // Fail
-            return '';
-        }
-        
-        public function shortcodeMiradorItems($args, $view) {
-            // Styles
-            $styles = isset($args['style']) ? $args['style'] : '';
-            // Multiple: Rip arguments from existing [items] shortcode, for finding items
-            if (isset($args['ids'])) {
-                $params = array();
-                if (isset($args['is_featured'])) {
-                    $params['featured'] = $args['is_featured'];
-                }
-                if (isset($args['has_image'])) {
-                    $params['hasImage'] = $args['has_image'];
-                }
-                if (isset($args['collection'])) {
-                    $params['collection'] = $args['collection'];
-                }
-                if (isset($args['item_type'])) {
-                    $params['item_type'] = $args['item_type'];
-                }
-                if (isset($args['tags'])) {
-                    $params['tags'] = $args['tags'];
-                }
-                if (isset($args['user'])) {
-                    $params['user'] = $args['user'];
-                }
-                if (isset($args['ids'])) {
-                    $params['range'] = $args['ids'];
-                }
-                if (isset($args['sort'])) {
-                    $params['sort_field'] = $args['sort'];
-                }
-                if (isset($args['order'])) {
-                    $params['sort_dir'] = $args['order'];
-                }
-                if (isset($args['num'])) {
-                    $limit = $args['num'];
-                } else {
-                    $limit = 10; 
-                }
-                $items = get_records('Item', $params, $limit);
-                $item_ids = array();
-                foreach ($items as $item) {
-                    $item_ids[] = $item->id;
-                }
-                // Add iframe
-                return '<iframe src="' . public_full_url(array(), 'iiifitems_exhibit_mirador', array('items' => join(',', $item_ids))) . '" style="width:100%;height:400px;' . $styles . '" allowfullscreen="true"></iframe>';
-            }
-            // Single: View quick-view manifest of the item
-            if (isset($args['id'])) {
-                $id = $args['id'];
-                $item = get_record_by_id('Item', $id);
-                if ($item) {
-                    return '<iframe src="' . public_full_url(array('things' => 'items', 'id' => $id), 'iiifitems_mirador') . '" style="width:100%;height:400px;' . $styles . '" allowfullscreen="true"></iframe>';
-                }
-            }
-            // Fail
-            return '';
-        }
-        
-        public function shortcodeMiradorCollections($args, $view) {
-            // Styles
-            $styles = isset($args['style']) ? $args['style'] : '';
-            // Multiple: Rip arguments from existing [collections] shortcode, for finding collections
-            if (isset($args['ids'])) {
-                $params = array();
-                if (isset($args['ids'])) {
-                    $params['range'] = $args['ids'];
-                }
-                if (isset($args['sort'])) {
-                    $params['sort_field'] = $args['sort'];
-                }
-                if (isset($args['order'])) {
-                    $params['sort_dir'] = $args['order'];
-                }
-                if (isset($args['is_featured'])) {
-                    $params['featured'] = $args['is_featured'];
-                }
-                if (isset($args['num'])) {
-                    $limit = $args['num'];
-                } else {
-                    $limit = 10; 
-                }
-                $collections = get_records('Collection', $params, $limit);
-                $collection_urls = array();
-                foreach ($collections as $collection) {
-                    $collection_urls[] = public_full_url(array('things' => 'collections', 'id' => $collection->id), 'iiifitems_manifest');
-                }
-                // Add iframe
-                return '<iframe src="' . public_full_url(array(), 'iiifitems_exhibit_mirador', array('u' => $collection_urls)) . '" style="width:100%;height:400px;' . $styles . '" allowfullscreen="true"></iframe>';
-            }
-            // Single: View quick-view manifest of the collection
-            if (isset($args['id'])) {
-                $id = $args['id'];
-                $collection = get_record_by_id('Collection', $id);
-                if ($collection) {
-                    return '<iframe src="' . public_full_url(array('things' => 'collections', 'id' => $id), 'iiifitems_mirador') . '" style="width:100%;height:400px;' . $styles . '" allowfullscreen="true"></iframe>';
-                }
-            }
-            // Fail
-            return '';
-        }
-        
-        public function shortcodeMirador($args, $view) {
-            // Styles
-            $styles = isset($args['style']) ? $args['style'] : '';
-            // Grab URL arguments
-            $urls = isset($args['urls']) ? explode(';', $args['urls']) : array();
-            // Add iframe
-            return '<iframe src="' . public_full_url(array(), 'iiifitems_exhibit_mirador', array('u' => $urls)) . '" style="width:100%;height:400px;' . $styles . '" allowfullscreen="true"></iframe>';
         }
 }
 ?>
