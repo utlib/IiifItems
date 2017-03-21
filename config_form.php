88fcc3f5673dbc80a80fd73fb795ebc220f78faa
Dickson Law <dickson.law@utoronto.ca>
2017-03-21 10:29:11 -0400

    Moved off forms

--- config_form.php
+++ config_form.php
@@ -1,5 +1,4 @@
 <?php
-    require_once IIIF_ITEMS_DIRECTORY . '/forms/Config.php';
     $form = new IiifItems_Form_Config();
     $form->removeDecorator('Form');
     $form->removeDecorator('Zend_Form_Decorator_Form');
