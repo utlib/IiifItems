fc1e0b2bc2c1526168e4a2d60cebc7665cc08743
Dickson Law <dickson.law@utoronto.ca>
2017-03-24 14:49:02 -0400

    Documented models and tables

--- models/IiifItems/JobStatus.php
+++ models/IiifItems/JobStatus.php
@@ -1,5 +1,66 @@
 <?php
 
+/**
+ * A record for the status of background running jobs.
+ */
 class IiifItems_JobStatus extends Omeka_Record_AbstractRecord {
-    public $id, $source, $dones, $skips, $fails, $status, $progress, $total, $added, $modified;
+    /**
+     * The database primary key.
+     * @var integer
+     */
+    public $id;
+    
+    /**
+     * The description of the job that this job status describes.
+     * @var string
+     */
+    public $source;
+    
+    /**
+     * Number of subtasks successfully done.
+     * @var integer
+     */
+    public $dones;
+    
+    /**
+     * Number of subtasks skipped.
+     * @var integer
+     */
+    public $skips;
+    
+    /**
+     * Number of subtasks failed.
+     * @var integer
+     */
+    public $fails;
+    
+    /**
+     * Description of the status of the job.
+     * @var string
+     */
+    public $status;
+    
+    /**
+     * Number of subtasks completed so far.
+     * @var integer
+     */
+    public $progress;
+    
+    /**
+     * Total number of subtasks forecasted for this job.
+     * @var integer
+     */
+    public $total;
+    
+    /**
+     * When the job was started.
+     * @var DateTime
+     */
+    public $added;
+    
+    /**
+     * When the job status was last updated.
+     * @var DateTime
+     */
+    public $modified;
 }
--- models/Table/IiifItems/JobStatus.php
+++ models/Table/IiifItems/JobStatus.php
@@ -1,4 +1,7 @@
 <?php
 
+/**
+ * Table class for accessing background job statuses.
+ */
 class Table_IiifItems_JobStatus extends Omeka_Db_Table {
 }
