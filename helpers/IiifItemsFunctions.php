7af570cc6bcd6cc5746a6026e23bb807903c50fd
Dickson Law <dickson.law@utoronto.ca>
2017-04-21 11:39:50 -0400

    Fix cache clearing for collections

--- helpers/IiifItemsFunctions.php
+++ helpers/IiifItemsFunctions.php
@@ -105,7 +105,7 @@ function clear_iiifitems_cache_values_for($record, $bubble=true) {
             break;
             case 'Collection':
                 if ($parentCollectionId = raw_iiif_metadata($record, 'iiifitems_collection_parent_element')) {
-                    if ($parentCollection = $db->getTable('Collection')->find($parentCollectionId)) {
+                    if ($parentCollection = IiifItems_Util_Collection::findParentFor($record)) {
                         clear_iiifitems_cache_values_for($parentCollection, false);
                     }
                 }
