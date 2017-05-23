4faf6f80e8a161f9da49b4a334cb6a1cef3f829a
Dickson Law <dickson.law@utoronto.ca>
2017-05-23 13:48:32 -0400

    Add support for exhibition block level manifests, integrate with IIIF Items exhibit block

--- views/shared/exhibit_layouts/iiifitem/form.php
+++ views/shared/exhibit_layouts/iiifitem/form.php
@@ -5,4 +5,6 @@ $options = $block->getOptions();
 <div class="selected-items">
     <h4><?php echo __('Items'); ?></h4>
     <?php echo $this->exhibitFormAttachments($block); ?>
+    <h4><?php echo __('Description'); ?></h4>
+    <?php echo $this->exhibitFormText($block); ?>
 </div>
