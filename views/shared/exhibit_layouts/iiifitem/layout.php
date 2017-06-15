6d9f95258df9e94740d8f18a9e88225503f41f56
Dickson Law <dickson.law@utoronto.ca>
2017-06-15 20:52:20 +0200

    Fix missing text element for IIIF Items block

--- views/shared/exhibit_layouts/iiifitem/layout.php
+++ views/shared/exhibit_layouts/iiifitem/layout.php
@@ -2,6 +2,7 @@
 
 <div class="mirador-item">
     <iframe src="<?php echo public_full_url(array('things' => 'exhibit_page_blocks', 'id' => $attachments[0]->block_id), 'iiifitems_mirador'); ?>" allowfullscreen="allowfullscreen"></iframe>
+    <p><?php echo $text; ?></p>
 </div>
 
 <?php endif; ?>
