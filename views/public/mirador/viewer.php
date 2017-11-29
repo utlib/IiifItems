459836860d9ed192f06730ed4e700f59c979c10a
Dickson Law <dickson.law@utoronto.ca>
2017-11-29 16:16:50 -0500

    Cherry-pick localization override for Mirador, add basic translations for English and Chinese for reference

--- views/public/mirador/viewer.php
+++ views/public/mirador/viewer.php
@@ -23,6 +23,7 @@
         Mirador({
             "id": "viewer",
             "buildPath": "<?php echo html_escape($mirador_path) . '/'; ?>",
+            "language": "<?php echo Zend_Registry::get('bootstrap')->getResource('Locale')->toString(); ?>",
             "layout": "1",
             "data": [
                 <?php if ($type != 'collections' || !IiifItems_Util_Collection::isCollection($thing)) : ?>
